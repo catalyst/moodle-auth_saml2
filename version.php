88bf365097ccabc367fbb8ad27fe685dd351bb01
Adam Riddell <adamr@catalyst-au.net>
2016-03-15 13:53:24 +1100

    Add call to pull in complete user data for session

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016020300;    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2015113001;    // Match release exactly to version.
+$plugin->version   = 2016030300;    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2016030300;    // Match release exactly to version.
 $plugin->requires  = 2014050800;    // Requires this Moodle version.
 $plugin->component = 'auth_saml2';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
