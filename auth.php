5a1ff3a6dd5224c443fadbb26f2135dce208a72e
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2016-10-12 17:54:05 +1100

    Fixes Issue #46. Updating custom user profile fields.

--- auth.php
+++ auth.php
@@ -285,29 +285,16 @@ class auth_plugin_saml2 extends auth_plugin_base {
                     $updateonlogin = $mapconfig->{'field_updatelocal_'.$field} === 'onlogin';
 
                     if ($newuser || $updateonlogin) {
-                        // Determine which fields are custom profile fields and act accordingly.
-                        if (in_array($field, $customprofilefields)) {
 
-                            // Check to see if the attributes exist before mapping the data.
-                            if (array_key_exists($attr, $attributes)) {
-                                // TODO data validation should be done before saving custom fields back currently if the SAML data is not of the correct type an error is displayed.
-                                // Handing an empty array of attributes.
-                                if (!empty($attributes[$attr])) {
-                                    $user->{'profile_field_'.$field} = $attributes[$attr][0];
-                                }
-                            }
-
-                        } else {
-                            // Basic error handling, check to see if the attributes exist before mapping the data.
-                            if (array_key_exists($attr, $attributes)) {
-                                // Handing an empty array of attributes.
-                                if (!empty($attributes[$attr])) {
-                                    $user->$field = $attributes[$attr][0];
-                                }
+                        // Basic error handling, check to see if the attributes exist before mapping the data.
+                        if (array_key_exists($attr, $attributes)) {
+                            // Handing an empty array of attributes.
+                            if (!empty($attributes[$attr])) {
+                                // Custom profile fields have the prefix profile_field_ and will be saved as profile field data.
+                                $user->$field = $attributes[$attr][0];
+                                $touched = true;
                             }
                         }
-
-                        $touched = true;
                     }
                 }
             }
