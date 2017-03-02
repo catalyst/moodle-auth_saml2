8f77c8331f106d829cf5b1ab3eaea20ad8143852
Sam Chaffee <sam@moodlerooms.com>
2017-03-02 12:35:18 -0700

    auth_saml2: Add ability to override SSL-related cURL options

--- tests/metadata_fetcher_test.php
+++ tests/metadata_fetcher_test.php
@@ -95,4 +95,41 @@ class auth_saml2_metadata_fetcher_testcase extends advanced_testcase {
             $this->assertContains('Metadata fetch failed: Unknown cURL error', $e->getMessage());
         }
     }
+
+    public function test_fetch_metadata_override_ssl_options() {
+        global $CFG;
+
+        $this->resetAfterTest(true);
+
+        $options = [
+            'CURLOPT_SSL_VERIFYPEER' => false,
+            'CURLOPT_SSL_VERIFYHOST' => false,
+            'CURLOPT_CONNECTTIMEOUT' => 20,
+            'CURLOPT_FOLLOWLOCATION' => 1,
+            'CURLOPT_MAXREDIRS'      => 5,
+            'CURLOPT_TIMEOUT'        => 300,
+            'CURLOPT_RETURNTRANSFER' => true,
+            'CURLOPT_NOBODY'         => false,
+        ];
+        $url = 'https://fakeurl.localhost';
+        if (!is_array($CFG->forced_plugin_settings)) {
+            $CFG->forced_plugin_settings = [];
+        }
+        if (!array_key_exists('auth/saml2', $CFG->forced_plugin_settings)) {
+            $CFG->forced_plugin_settings['auth/saml2'] = [];
+        }
+        $CFG->forced_plugin_settings['auth/saml2']['CURLOPT_SSL_VERIFYPEER'] = 0;
+        $CFG->forced_plugin_settings['auth/saml2']['CURLOPT_SSL_VERIFYHOST'] = 0;
+
+        $curl = $this->prophesize('curl');
+
+        $fetcher = new metadata_fetcher();
+
+        // Prophecy asserts that indeed the options passed to curl->get() from fetch() contained forced settings.
+        $curl->get($url, $options)->willReturn('Some error');
+        $curl->get_info()->willReturn(['http_code' => 200]);
+        $curl->get_errno()->willReturn(0);
+
+        $fetcher->fetch($url, $curl->reveal());
+    }
 }
