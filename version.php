0ea548123d5c4853d00ed8886aaa390f2c1de4a5
Dustin Brisebois <dustin.brisebois@lambdasolutions.net>
2017-04-27 10:40:17 -0700

    Update version.php

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017042701;    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017042702;    // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release   = 2017042700;    // Match release exactly to version.
 $plugin->requires  = 2014050800;    // Requires this Moodle version.
 $plugin->component = 'auth_saml2';  // Full name of the plugin (used for diagnostics).
