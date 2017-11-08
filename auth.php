c6237b251c9476180f8af900fd3d76bbfa387940
F Devine <francis@catalyst.net.nz>
2017-11-09 01:44:03 +1300

    Add ability to configure SSPHP to log to file (#156)
    The debug messages from SSPHP can be very verbose, so having this as an
    option is very useful for extensive debugging sessions

--- auth.php
+++ auth.php
@@ -51,6 +51,8 @@ class auth_plugin_saml2 extends auth_plugin_base {
         'showidplink'     => true,
         'alterlogout'     => '',
         'nameidasattrib'  => 0,
+        'logtofile'       => 0,
+        'logdir'          => '/tmp/',
     );
 
     /**
