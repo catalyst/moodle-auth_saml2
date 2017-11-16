da4246474c35ca8f2ffcd84a317ce5f57b93d71f
Brendan Heywood <brendan@catalyst-au.net>
2017-11-16 14:05:41 +1100

    Version bump

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017092701;    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2017092701;    // Match release exactly to version.
+$plugin->version   = 2017111600;    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2017111600;    // Match release exactly to version.
 $plugin->requires  = 2017051500;    // Requires this Moodle version.
 $plugin->component = 'auth_saml2';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
