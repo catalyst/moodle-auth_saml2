261c132657cb85866050b7c992b3eee31507acf3
Daniel Thee Roperto <daniel@theeroperto.com>
2018-03-20 13:01:53 +1100

    Version 2018032000

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018031300;    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2018031300;    // Match release exactly to version.
+$plugin->version   = 2018032000;    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2018032000;    // Match release exactly to version.
 $plugin->requires  = 2014051200;    // Requires this Moodle version. (2.7)
 $plugin->component = 'auth_saml2';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
