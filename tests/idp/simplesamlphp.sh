12b19ee3cb6d03aca2f0148a2c39ce729e652ca1
Dan Marsden <dan@danmarsden.com>
2019-04-16 09:49:28 +1200

    Use simplesaml 1.16.3 for test idp.

--- tests/idp/simplesamlphp.sh
+++ tests/idp/simplesamlphp.sh
@@ -3,20 +3,20 @@ set -eo xtrace
 
 # Download
 pushd ~
-curl -L https://github.com/simplesamlphp/simplesamlphp/releases/download/v1.15.1/simplesamlphp-1.15.1.tar.gz | tar zx
-cd simplesamlphp-1.15.1
+curl -L https://github.com/simplesamlphp/simplesamlphp/releases/download/v1.16.3/simplesamlphp-1.16.3.tar.gz | tar zx
+cd simplesamlphp-1.16.3
 composer install
 popd
 
 # Config
-touch ~/simplesamlphp-1.15.1/modules/exampleauth/enable
-mkdir -p ~/simplesamlphp-1.15.1/cert
-cp -v tests/idp/authsources.idp ~/simplesamlphp-1.15.1/config/authsources.php
-cp -v tests/idp/config.idp ~/simplesamlphp-1.15.1/config/config.php
-cp -v tests/idp/crt ~/simplesamlphp-1.15.1/cert/server.crt
-cp -v tests/idp/pem ~/simplesamlphp-1.15.1/cert/server.pem
-cp -v tests/idp/sp-remote.idp ~/simplesamlphp-1.15.1/metadata/saml20-sp-remote.php
+touch ~/simplesamlphp-1.16.3/modules/exampleauth/enable
+mkdir -p ~/simplesamlphp-1.16.3/cert
+cp -v tests/idp/authsources.idp ~/simplesamlphp-1.16.3/config/authsources.php
+cp -v tests/idp/config.idp ~/simplesamlphp-1.16.3/config/config.php
+cp -v tests/idp/crt ~/simplesamlphp-1.16.3/cert/server.crt
+cp -v tests/idp/pem ~/simplesamlphp-1.16.3/cert/server.pem
+cp -v tests/idp/sp-remote.idp ~/simplesamlphp-1.16.3/metadata/saml20-sp-remote.php
 
 # Run
-cd ~/simplesamlphp-1.15.1/www
+cd ~/simplesamlphp-1.16.3/www
 php -S localhost:8001 2> /dev/null &
