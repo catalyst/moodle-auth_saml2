d7c0500f74fe79ecf27f2425892e9aa67ce02b95
Brendan Heywood <brendan@catalyst-au.net>
2019-05-10 11:29:34 +1200

    Rely only on moodle registered shutdown handlers to save the session (#327)

--- extlib/simplesamlphp/lib/SimpleSAML/Session.php
+++ extlib/simplesamlphp/lib/SimpleSAML/Session.php
@@ -488,6 +488,10 @@ class SimpleSAML_Session implements Serializable
 
         $this->dirty = true;
 
+        // Moodle auth_saml2 hack, because we register a shutdown handler in
+        // moodle in the constructor we don't need to register a callback here.
+        return;
+
         if ($this->callback_registered) {
             // we already have a shutdown callback registered for this object, no need to add another one
             return;
@@ -504,7 +508,9 @@ class SimpleSAML_Session implements Serializable
      */
     public function __destruct()
     {
-        $this->save();
+        // Moodle auth_saml2 hack, we don't need to save here because we have
+        // a custom shutdown handle registered with moodle.
+        // $this->save();
     }
 
     /**
