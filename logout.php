5ca3e07c8376016df9b89f9dd27a95def835f9a7
Luiz Rossi <lh.rossi@cognitivabrasil.com.br>
2022-08-25 17:37:23 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -27,7 +27,7 @@ require_once(__DIR__ . '/../../config.php');
 // @codingStandardsIgnoreEnd
 require('setup.php');
 
-// Generally this page is not used as the normla moodle logout_hook
+// Generally this page is not used as the normal moodle logout_hook
 // doe everything needed. This page is used when someone is logged
 // into SAML, but not into Moodle, and needs to logout of SAML in
 // order to properly re-log into Moodle. It's a rare edge case
