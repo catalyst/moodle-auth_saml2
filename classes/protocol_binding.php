ebd2554ddbb6c250778c86387b7d205258d03eb9
Lars Mikal Rogne <lars.mikal.rogne@no.experis.com>
2024-05-06 08:30:08 +0200

    Remove bindings which are not relevant in the context of IdP response to SP.

--- classes/protocol_binding.php
+++ classes/protocol_binding.php
@@ -16,8 +16,7 @@ use coding_exception;
 class protocol_binding {
     public const HTTP_POST = 0;
     public const HTTP_ARTIFACT = 1;
-    public const HOK_SSO = 2;
-    public const HTTP_REDIRECT = 3;
+
     /**
      * Map the internal id of the protocol bindings to the actual binding string.
      * @param int $id
@@ -30,10 +29,6 @@ class protocol_binding {
                 return 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST';
             case self::HTTP_ARTIFACT:
                 return 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact';
-            case self::HOK_SSO:
-                return 'urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser';
-            case self::HTTP_REDIRECT:
-                return 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect';
             default:
                 throw new coding_exception('Invalid binding');
         }
@@ -42,8 +37,6 @@ class protocol_binding {
         return [
             self::HTTP_POST => 'HTTP Post',
             self::HTTP_ARTIFACT => 'HTTP Artifact',
-            self::HOK_SSO => 'Holder-of-Key Web Browser SSO',
-            self::HTTP_REDIRECT => 'HTTP Redirect',
         ];
     }
 }
