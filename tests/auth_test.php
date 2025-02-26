34a395d8aa060f6a2f561a1128e7d30f8a152fb3
Sasha Anastasi <sasha.anastasi@catalyst.net.nz>
2025-02-26 15:23:31 +1300

    make test data provider methods static

--- tests/auth_test.php
+++ tests/auth_test.php
@@ -798,7 +798,7 @@ class auth_saml2_auth_test extends \core_phpunit\testcase {
      *
      * @return array of testcases
      */
-    public function provider_should_login_redirect(): array {
+    public static function provider_should_login_redirect(): array {
         $midp = (new \moodle_url('/auth/saml2/selectidp.php'))->out();
         return [
             // Login normal, dual login on.
@@ -965,7 +965,7 @@ class auth_saml2_auth_test extends \core_phpunit\testcase {
      *
      * @return array
      */
-    public function provider_check_whitelisted_ip_redirect(): array {
+    public static function provider_check_whitelisted_ip_redirect(): array {
         return [
             'saml off, no ip, no redirect'              => ['off', '1.2.3.4', '', false],
             'saml not specified, junk, no redirect'     => [null, '1.2.3.4', 'qwer1234!@#qwer', false],
@@ -980,7 +980,7 @@ class auth_saml2_auth_test extends \core_phpunit\testcase {
      *
      * @return array
      */
-    public function provider_is_access_allowed(): array {
+    public static function provider_is_access_allowed(): array {
         return [
             '' => [[
                 ['uid' => 'test'], // User don't have groups attribute.
@@ -1204,7 +1204,7 @@ class auth_saml2_auth_test extends \core_phpunit\testcase {
      *
      * @return array of testcases
      */
-    public function provider_update_custom_user_profile_fields(): array {
+    public static function provider_update_custom_user_profile_fields(): array {
         return [
             [['testfield' => ['Test data']]],
             [['secondfield' => ['A different string']]],
@@ -1251,7 +1251,7 @@ class auth_saml2_auth_test extends \core_phpunit\testcase {
      *
      * @return array of testcases
      */
-    public function provider_missing_user_custom_profile_fields(): array {
+    public static function provider_missing_user_custom_profile_fields(): array {
         return array(
             array(['missingfield' => array('Test data')]),
             array(['secondfield' => array('A different string')]),
@@ -1290,7 +1290,7 @@ class auth_saml2_auth_test extends \core_phpunit\testcase {
      *
      * @return array of testcases
      */
-    public function provider_invalid_map_user_profile_fields(): array {
+    public static function provider_invalid_map_user_profile_fields(): array {
         return [
             [
                 ['field' => 'userame', 'mapping' => 'invalid'],
