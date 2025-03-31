a58cba7b244494cd360079fa0d06f24c776c1adc
Jay Oswald <jozzie52@gmail.com>
2025-03-31 11:17:17 +1100

    Update 3.9 Compatibility to reflect new 404 branch

--- version.php
+++ version.php
@@ -32,4 +32,4 @@ $plugin->requires  = 2017051509;    // Requires PHP 7, 2017051509 = T12. M3.3
                                     // for Totara 3.3 is the same as Moodle 3.3.
 $plugin->component = 'auth_saml2';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->supported = [39, 404];     // A range of branch numbers of supported moodle versions.
+$plugin->supported = [39, 403];     // A range of branch numbers of supported moodle versions.
