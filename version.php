49a23845f8d550a295093a70d4917b71690d9ddf
Jay Oswald <jozzie52@gmail.com>
2025-04-16 09:34:09 +1000

    401 branch only needs to support 401

--- version.php
+++ version.php
@@ -32,4 +32,4 @@ $plugin->requires  = 2022112800;    // Requires 4.1.
                                     // for Totara 3.3 is the same as Moodle 3.3.
 $plugin->component = 'auth_saml2';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->supported = [401, 403];     // A range of branch numbers of supported moodle versions.
+$plugin->supported = [401, 401];     // A range of branch numbers of supported moodle versions.
