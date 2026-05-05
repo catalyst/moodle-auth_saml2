3f9014d5904b4e5a0e0b0a02baef5ee652196909
Andre Menrath <andre.menrath@uni-graz.at>
2026-05-05 12:51:44 +0200

    fix: adjust usage of removed send function of SimpleSamlPHP XHTML template see https://github.com/simplesamlphp/simplesamlphp/pull/1281

--- sp/metadata.php
+++ sp/metadata.php
@@ -60,7 +60,7 @@ if (array_key_exists('output', $_REQUEST) && $_REQUEST['output'] == 'xhtml') {
 	$t->data['metadata'] = htmlspecialchars($xml);
 	$t->data['metadataflat'] = '$metadata[' . var_export($entityId, TRUE) . '] = ' . var_export($metaArray20, TRUE) . ';';
 	$t->data['metaurl'] = $source->getMetadataURL();
-	$t->show();
+	$t->send();
 } else {
 	// header('Content-Type: application/samlmetadata+xml');
 	header('Content-Type: text/xml');
