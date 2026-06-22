3537aa325b8b3662985442aeaabfdf89d9ee5f11
Peter Burnett <peterburnett@catalyst-au.net>
2026-06-22 16:37:02 +1000

    fix: improve wantsurl handling from loginpage list

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025040403;    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2025040403;    // Match release exactly to version.
+$plugin->version   = 2025040404;    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2025040404;    // Match release exactly to version.
 $plugin->requires  = 2024100700;    // Requires Moodle 4.5
 $plugin->component = 'auth_saml2';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
